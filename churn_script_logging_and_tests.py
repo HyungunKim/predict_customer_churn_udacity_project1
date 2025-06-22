cc17a3d72ac5c64946d9101d3dda4d18126c1dbf
Hyunkun Kim <hyungun.kimm@gmail.com>
2025-06-22 20:11:25 +0900

    manual clean from pylint messages Your code has been rated at 9.81/10 (previous run: 8.50/10, +1.30)

--- churn_script_logging_and_tests.py
+++ churn_script_logging_and_tests.py
@@ -1,10 +1,17 @@
+"""
+A test module for the customer churn prediction library.
+
+This module contains unit tests for all functions in the churn_library.py module,
+including data import, EDA, feature engineering, model training and evaluation.
+Each test function includes logging to track success and failures of the tests.
+"""
 import os
+import sys
 import logging
-import churn_library as cls
 import pytest
 import joblib
 import pandas as pd
-import numpy as np
+import churn_library as cls
 
 logging.basicConfig(
     filename='./logs/churn_library_test.log',
@@ -82,7 +89,7 @@ def predict_models():
     return cls.predict_models
 
 
-def test_import(import_data):
+def test_import(import_data): # pylint: disable=redefined-outer-name
     '''
     test data import - this example is completed for you to assist with the other test functions
     '''
@@ -102,7 +109,7 @@ def test_import(import_data):
         raise err
 
 
-def test_eda(perform_eda):
+def test_eda(perform_eda): # pylint: disable=redefined-outer-name
     '''
     test perform eda function
     '''
@@ -128,7 +135,7 @@ def test_eda(perform_eda):
         raise err
 
 
-def test_encoder_helper(encoder_helper):
+def test_encoder_helper(encoder_helper): # pylint: disable=redefined-outer-name
     '''
     test encoder helper
     '''
@@ -169,7 +176,7 @@ def test_encoder_helper(encoder_helper):
         raise err
 
 
-def test_perform_feature_engineering(perform_feature_engineering):
+def test_perform_feature_engineering(perform_feature_engineering): # pylint: disable=redefined-outer-name
     '''
     test perform_feature_engineering
     '''
@@ -187,19 +194,19 @@ def test_perform_feature_engineering(perform_feature_engineering):
         df = cls.encoder_helper(df, category_lst)
 
         # Perform feature engineering
-        X_train, X_test, y_train, y_test = perform_feature_engineering(df)
+        x_train, x_test, y_train, y_test = perform_feature_engineering(df)
         logging.info("Testing perform_feature_engineering: SUCCESS")
 
         # Check that the outputs are not empty
         try:
-            assert X_train.shape[0] > 0
-            assert X_test.shape[0] > 0
+            assert x_train.shape[0] > 0
+            assert x_test.shape[0] > 0
             assert len(y_train) > 0
             assert len(y_test) > 0
 
             # Check that the train/test split was done correctly
-            assert X_train.shape[0] == len(y_train)
-            assert X_test.shape[0] == len(y_test)
+            assert x_train.shape[0] == len(y_train)
+            assert x_test.shape[0] == len(y_test)
 
             logging.info(
                 "Testing perform_feature_engineering outputs: SUCCESS")
@@ -213,7 +220,7 @@ def test_perform_feature_engineering(perform_feature_engineering):
         raise err
 
 
-def test_train_models(train_models):
+def test_train_models(train_models): # pylint: disable=redefined-outer-name
     '''
     test train_models
     '''
@@ -231,11 +238,11 @@ def test_train_models(train_models):
         df = cls.encoder_helper(df, category_lst)
 
         # Perform feature engineering
-        X_train, X_test, y_train, y_test = cls.perform_feature_engineering(df)
+        x_train, x_test, y_train, y_test = cls.perform_feature_engineering(df)
 
         # Train models
         y_train_preds_lr, y_train_preds_rf, y_test_preds_lr, y_test_preds_rf = train_models(
-            X_train, X_test, y_train, y_test)
+            x_train, x_test, y_train, y_test)
         logging.info("Testing train_models: SUCCESS")
 
         # Check that the outputs are not empty
@@ -264,8 +271,8 @@ def test_train_models(train_models):
 
             # Check feature importance plot
             rfc = joblib.load('./models/rfc_model.pkl')
-            X_all = pd.concat([X_train, X_test], axis=0)
-            cls.feature_importance_plot(rfc, X_all.iloc[::10], './images')
+            x_all = pd.concat([x_train, x_test], axis=0)
+            cls.feature_importance_plot(rfc, x_all.iloc[::10], './images')
 
             assert os.path.isfile('./images/feature_importance.png')
             assert os.path.isfile('./images/shap_feature_importance.png')
@@ -281,7 +288,7 @@ def test_train_models(train_models):
         raise err
 
 
-def test_classification_report_image(classification_report_image):
+def test_classification_report_image(classification_report_image): # pylint: disable=redefined-outer-name
     '''
     test classification_report_image function
     '''
@@ -294,16 +301,16 @@ def test_classification_report_image(classification_report_image):
                                  'Marital_Status',
                                  'Income_Category',
                                  'Card_Category'])
-        X_train, X_test, y_train, y_test = cls.perform_feature_engineering(df)
+        x_train, x_test, y_train, y_test = cls.perform_feature_engineering(df)
 
         # Train models or load existing models
         if os.path.exists(
                 './models/rfc_model.pkl') and os.path.exists('./models/lrc_model.pkl'):
             y_train_preds_lr, y_train_preds_rf, y_test_preds_lr, y_test_preds_rf = cls.predict_models(
-                X_train, X_test)
+                x_train, x_test)
         else:
             y_train_preds_lr, y_train_preds_rf, y_test_preds_lr, y_test_preds_rf = cls.train_models(
-                X_train, X_test, y_train, y_test)
+                x_train, x_test, y_train, y_test)
 
         # Create ModelResults objects
         # Note: The order of parameters here matches the usage in the main function
@@ -336,7 +343,7 @@ def test_classification_report_image(classification_report_image):
         raise err
 
 
-def test_feature_importance_plot(feature_importance_plot):
+def test_feature_importance_plot(feature_importance_plot): # pylint: disable=redefined-outer-name
     '''
     test feature_importance_plot function
     '''
@@ -349,18 +356,18 @@ def test_feature_importance_plot(feature_importance_plot):
                                  'Marital_Status',
                                  'Income_Category',
                                  'Card_Category'])
-        X_train, X_test, y_train, y_test = cls.perform_feature_engineering(df)
+        x_train, x_test, y_train, y_test = cls.perform_feature_engineering(df)
 
         # Load model
         if not os.path.exists('./models/rfc_model.pkl'):
             # Train model if it doesn't exist
-            cls.train_models(X_train, X_test, y_train, y_test)
+            cls.train_models(x_train, x_test, y_train, y_test)
 
         rfc = joblib.load('./models/rfc_model.pkl')
 
         # Call the function to test
-        X_all = pd.concat([X_train, X_test], axis=0)
-        feature_importance_plot(rfc, X_all.iloc[::10], './images')
+        x_all = pd.concat([x_train, x_test], axis=0)
+        feature_importance_plot(rfc, x_all.iloc[::10], './images')
         logging.info("Testing feature_importance_plot: SUCCESS")
 
         # Check that the output files exist
@@ -379,7 +386,7 @@ def test_feature_importance_plot(feature_importance_plot):
         raise err
 
 
-def test_predict_models(predict_models):
+def test_predict_models(predict_models): # pylint: disable=redefined-outer-name
     '''
     test predict_models function
     '''
@@ -392,17 +399,17 @@ def test_predict_models(predict_models):
                                  'Marital_Status',
                                  'Income_Category',
                                  'Card_Category'])
-        X_train, X_test, y_train, y_test = cls.perform_feature_engineering(df)
+        x_train, x_test, y_train, y_test = cls.perform_feature_engineering(df)
 
         # Ensure models exist
         if not (os.path.exists('./models/rfc_model.pkl')
                 and os.path.exists('./models/lrc_model.pkl')):
             # Train models if they don't exist
-            cls.train_models(X_train, X_test, y_train, y_test)
+            cls.train_models(x_train, x_test, y_train, y_test)
 
         # Call the function to test
         y_train_preds_lr, y_train_preds_rf, y_test_preds_lr, y_test_preds_rf = predict_models(
-            X_train, X_test)
+            x_train, x_test)
         logging.info("Testing predict_models: SUCCESS")
 
         # Check that the outputs are not empty
@@ -413,10 +420,10 @@ def test_predict_models(predict_models):
             assert len(y_test_preds_rf) > 0
 
             # Check that the predictions have the correct shape
-            assert len(y_train_preds_lr) == X_train.shape[0]
-            assert len(y_train_preds_rf) == X_train.shape[0]
-            assert len(y_test_preds_lr) == X_test.shape[0]
-            assert len(y_test_preds_rf) == X_test.shape[0]
+            assert len(y_train_preds_lr) == x_train.shape[0]
+            assert len(y_train_preds_rf) == x_train.shape[0]
+            assert len(y_test_preds_lr) == x_test.shape[0]
+            assert len(y_test_preds_rf) == x_test.shape[0]
 
             logging.info("Testing predict_models outputs: SUCCESS")
         except AssertionError as err:
@@ -439,4 +446,4 @@ if __name__ == "__main__":
     # Run pytest
     retcode = pytest.main([os.path.join(os.path.dirname(
         __file__), 'churn_script_logging_and_tests.py')])
-    exit(retcode)
+    sys.exit(retcode)
