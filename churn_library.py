d42986c189572063d60d5d22df7123d5de41a376
Hyunkun Kim <hyungun.kimm@gmail.com>
2025-06-22 19:06:37 +0900

    remove unused imports

--- churn_library.py
+++ churn_library.py
@@ -26,7 +26,6 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import normalize
 import shap
 import joblib
 import pandas as pd
